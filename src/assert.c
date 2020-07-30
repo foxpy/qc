4c0a146a9c818980ca2e472f2f5cae4cc86aa11c
Murad <muradkarammaev@gmail.com>
2020-07-30 09:11:50 +0300

    better assertion fail message

--- src/assert.c
+++ src/assert.c
@@ -3,7 +3,7 @@
 
 void qc_assert(bool cond, char *err_msg) {
     if (!cond) {
-        fprintf(stderr, "%s:%d", __FILE__, __LINE__);
+        fprintf(stderr, "Assertion failed: %s:%d, ", __FILE__, __LINE__);
         die(err_msg);
     }
 }
