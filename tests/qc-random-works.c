22ddac97a66b0ec0d877a0aef40d627baa5f64ed
Murad <muradkarammaev@gmail.com>
2020-08-11 18:26:51 +0300

    fix broken test for random

--- tests/qc-random-works.c
+++ tests/qc-random-works.c
@@ -11,16 +11,18 @@ struct test_random_data {
     double f64;
 };
 
-void populate(struct test_random_data *t, qc_rnd *s) {
-    t->u8 = qc_rnd8(s);
-    t->u16 = qc_rnd16(s);
-    t->u32 = qc_rnd32(s);
-    t->u64 = qc_rnd64(s);
-    t->f32 = qc_rnd_fp32(s);
-    t->f64 = qc_rnd_fp64(s);
+static void populate(struct test_random_data *t) {
+    qc_rnd s;
+    qc_rnd_init(&s);
+    t->u8 = qc_rnd8(&s);
+    t->u16 = qc_rnd16(&s);
+    t->u32 = qc_rnd32(&s);
+    t->u64 = qc_rnd64(&s);
+    t->f32 = qc_rnd_fp32(&s);
+    t->f64 = qc_rnd_fp64(&s);
 }
 
-size_t compare(struct test_random_data *t1, struct test_random_data *t2) {
+static size_t compare(struct test_random_data *t1, struct test_random_data *t2) {
     return memcmp(&t1->u8, &t2->u8, sizeof(uint8_t)) |
            memcmp(&t1->u16, &t2->u16, sizeof(uint16_t)) |
            memcmp(&t1->u32, &t2->u32, sizeof(uint32_t)) |
@@ -30,11 +32,10 @@ size_t compare(struct test_random_data *t1, struct test_random_data *t2) {
 }
 
 int main() {
-    qc_rnd s1, s2, s3;
     struct test_random_data t1, t2, t3;
-    qc_rnd_init(&s1);
-    qc_rnd_init(&s2);
-    qc_rnd_init(&s3);
+    populate(&t1);
+    populate(&t2);
+    populate(&t3);
     qc_assert(compare(&t1, &t2), "random is not seeded well");
     qc_assert(compare(&t1, &t3), "random is not seeded well");
     qc_assert(compare(&t2, &t3), "random is not seeded well");
