c71b2856800925cceb226e82f99a86fee11e657b
Murad <muradkarammaev@gmail.com>
2020-08-13 23:59:57 +0300

    simplify platform random

--- src/platform_random.c
+++ src/platform_random.c
@@ -1,7 +1,5 @@
-#ifdef __MINGW32__
+#if defined _WIN32
 #   include <wincrypt.h>
-#elif defined _WIN32
-#   define _CRT_RAND_S
 #elif defined __linux__ || defined __ANDROID_API__
 #   include <sys/syscall.h>
 #   include <unistd.h>
@@ -12,13 +10,10 @@
 #include <stdlib.h>
 
 void qc_rnd_init(qc_rnd *state) {
-#   ifdef __MINGW32__
+#   if defined _WIN32
         HCRYPTPROV   hCryptProv;
         CryptAcquireContext(&hCryptProv, NULL, NULL, PROV_RSA_FULL, 0);
         CryptGenRandom(hCryptProv, sizeof(uint64_t), (PBYTE) &state->s64);
-#   elif defined _WIN32
-        rand_s(&state->s32[0]);
-        rand_s(&state->s32[1]);
 #   elif defined __ANDROID_API__
 #       if __ANDROID_API__ < 28
             arc4random_buf(&state->s64, sizeof(uint64_t));
