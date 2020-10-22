1f696464d1947879ad57d31af3d778ccea9ea4cc
Murad <muradkarammaev@gmail.com>
2020-10-22 15:23:53 +0300

    improve floating point random generator output quality
    in LFSR xorshift family, some least significant bits
    of output are low quality random so we throw them away

--- src/random.c
+++ src/random.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define DOUBLE_ONE_EXPONENT UINT64_C(0x3ff0000000000000)
-#define DOUBLE_FRACTION_MASK UINT64_C(0x000fffffffffffff)
+#define DOUBLE_FRACTION_SHIFT_LENGTH 12u
 
 static void xorshift64(uint64_t* s) {
     *s ^= *s << 21u;
@@ -24,7 +24,7 @@ double qc_rnd_fp64(qc_rnd* state) {
     xorshift64(&state->s64);
     qc_rnd tmp;
     memcpy(&tmp, state, sizeof(qc_rnd));
-    tmp.s64 &= DOUBLE_FRACTION_MASK;
+    tmp.s64 >>= DOUBLE_FRACTION_SHIFT_LENGTH;
     tmp.s64 |= DOUBLE_ONE_EXPONENT;
     tmp.fp64 -= 1.0;
     return tmp.fp64;
