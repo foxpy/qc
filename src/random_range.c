4729a0ff91d695705a168e2cfdacf76ef4096184
Murad <muradkarammaev@gmail.com>
2020-10-22 15:30:18 +0300

    improve qc_rnd_range64 output quality
    in LFSR xorshift family, some least significant bits
    of output are low quality random so we throw them away
    
    this commit is tightly coupled with 1f69646

--- src/random_range.c
+++ src/random_range.c
@@ -1,9 +1,14 @@
+#include <stdint.h>
 #include <assert.h>
 #include "qc.h"
 
 int64_t qc_rnd_range64(qc_rnd* state, int64_t low, int64_t high) {
     assert(high > low);
-    return (qc_rnd64(state) % (high - low + 1)) + low;
+    if (high - low < INT64_C(0x0000000000000100)) {
+        return ((qc_rnd64(state) >> 8u) % (high - low + 1)) + low;
+    } else {
+        return (qc_rnd64(state) % (high - low + 1)) + low;
+    }
 }
 
 double qc_rnd_range_fp64(qc_rnd* state, double low, double high) {
