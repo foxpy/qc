ce77597440fa75329c11012647343d3a1b3c8d37
Murad <muradkarammaev@gmail.com>
2020-12-11 17:06:25 +0300

    fix UB: va_list used to be passed without proper copying

--- src/string.c
+++ src/string.c
@@ -55,11 +55,17 @@ ptrdiff_t qc_asnprintf(char** dst, size_t mlimit, char const* format, ...) {
 }
 
 ptrdiff_t qc_vasprintf(char** dst, char const* format, va_list ap) {
-    return qc_vasnprintf(dst, INT_MAX, format, ap);
+    va_list args;
+    va_copy(args, ap);
+    return qc_vasnprintf(dst, INT_MAX, format, args);
+    va_end(args);
 }
 
 ptrdiff_t qc_vasnprintf(char** dst, size_t mlimit, char const* format, va_list ap) {
-    return qc_vasnprintf_impl(dst, 256, mlimit, format, ap);
+    va_list args;
+    va_copy(args, ap);
+    return qc_vasnprintf_impl(dst, 256, mlimit, format, args);
+    va_end(args);
 }
 
 qc_result qc_str_to_unsigned(char const* str, size_t* dst, char const** tail) {
