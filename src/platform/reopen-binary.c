bfd5d713572fc4bad2714d7fab4579ae34f7274e
Murad <muradkarammaev@gmail.com>
2020-12-18 10:31:29 +0300

    repeat d09f6df for reopen-binary.c: fix compilation issue

--- src/platform/reopen-binary.c
+++ src/platform/reopen-binary.c
@@ -9,7 +9,7 @@ void qc_reopen_binary_read(FILE *stream) {
 #   ifdef _WIN32
         _setmode(_fileno(stream), _O_BINARY);
 #   elif defined __ANDROID__
-        UNUSED(stream);
+        QC_UNUSED(stream);
 #   else
         freopen(NULL, "rb", stream);
 #   endif
@@ -19,7 +19,7 @@ void qc_reopen_binary_write(FILE *stream) {
 #   ifdef _WIN32
         _setmode(_fileno(stream), _O_BINARY);
 #   elif defined __ANDROID__
-        UNUSED(stream);
+        QC_UNUSED(stream);
 #   else
         freopen(NULL, "wb", stream);
 #   endif
