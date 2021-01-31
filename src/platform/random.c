ea7d470144ca13b18b3d7022d8d1238524accd38
Murad <muradkarammaev@gmail.com>
2021-01-31 21:00:20 +0300

    fix compilation on Android

--- src/platform/random.c
+++ src/platform/random.c
@@ -5,7 +5,11 @@
 #if defined _WIN32
 #    include "random/random_windows.c"
 #elif defined __ANDROID__
-#    include "random/random_android.c"
+#    if __ANDROID_API__ < 28
+#        include "random/random_bsd.c"
+#    else
+#        include "random/random_android.c"
+#    endif
 #elif defined __linux__
 #    include "random/random_linux.c"
 #elif defined(unix) || defined(__unix__) || defined(__unix) || defined(__APPLE__) || defined(__APPLE_CC__) || \
